503ebe4e62d45be6c0121b42396eac6d22868c1c
juanitopancracioperez-creator <juanitopancracioperez@gmail.com>
2025-10-23 09:33:16 -0600

    Add files via upload

---
+++ Razones trigonométricas .cxx
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double a, b, c, d;
+    
+
+    cout << "Ingrese el angulo en grados: " << endl;
+    cin >> a;
+
+    b = sin(c);
+     c = (a * 3.1415926535) / 180;
+
+    cout << "El angulo en R es: " << c << endl;
+    cout << "El seno del angulo es: " << b << endl;
+    
+    return 0;
+}
